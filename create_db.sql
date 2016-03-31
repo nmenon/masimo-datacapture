37c2fd2ed625df9863c7547ae44855dadd0ec661
Nishanth Menon <menon.nishanth@gmail.com>
2016-03-31 06:11:36 -0500

    create_db: reduce size of database by using better ranges and types
    the PI value is pretty much in a short range.. why allot a flexible
    float? we could have used DECIMAL here, but what the heck.. for the size
    involved, float is as good as any, right?
    
    ref: http://dev.mysql.com/doc/refman/5.5/en/storage-requirements.html
    
    Signed-off-by: Nishanth Menon <menon.nishanth@gmail.com>

--- create_db.sql
+++ create_db.sql
@@ -9,14 +9,14 @@ flush privileges;
 USE logmasimo;
 
 CREATE TABLE `data` (
-	`id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Unique Identifier',
-	`ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Time stamp of data',
-	`spo2` int(3) DEFAULT 0 COMMENT 'O2 % (peripheral capillary oxygen saturation)',
-	`bpm` int(3) DEFAULT 0 COMMENT 'Beats Per Minute',
-	`pi` float DEFAULT 0 COMMENT 'Perfusion Index',
-	`alarm` int(11) DEFAULT 0 COMMENT 'Raw Alarm value',
-	`exc` int(11) DEFAULT 0 COMMENT 'Raw Exception',
-	`exc1` int(11) DEFAULT 0 COMMENT 'Raw Exception 1',
+	`id` INTEGER(11) NOT NULL AUTO_INCREMENT COMMENT 'Unique Identifier',
+	`ts` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Time stamp of data',
+	`spo2` TINYINT(3) DEFAULT 0 COMMENT 'O2 % (peripheral capillary oxygen saturation)',
+	`bpm` SMALLINT(3) DEFAULT 0 COMMENT 'Beats Per Minute',
+	`pi` FLOAT(3,2) DEFAULT 0 COMMENT 'Perfusion Index',
+	`alarm` INTEGER(11) DEFAULT 0 COMMENT 'Raw Alarm value',
+	`exc` INTEGER(11) DEFAULT 0 COMMENT 'Raw Exception',
+	`exc1` INTEGER(11) DEFAULT 0 COMMENT 'Raw Exception 1',
 	primary key(id, ts),
 	key(spo2),
 	key(bpm)
