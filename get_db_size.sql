6569c4289491d55f5ff7dd58ff8d432a43242bb6
Nishanth Menon <menon.nishanth@gmail.com>
2016-04-01 04:41:28 -0500

    get_db_size: Add quick sql query to find data base sizes
    From:
    http://forums.mysql.com/read.php?108,201578,201578
    
    Signed-off-by: Nishanth Menon <menon.nishanth@gmail.com>

---
+++ get_db_size.sql
@@ -0,0 +1,6 @@
+SELECT table_schema "Data Base Name",
+	sum( data_length + index_length ) / 1024 /
+	1024 "Data Base Size in MB",
+	sum( data_free )/ 1024 / 1024 "Free Space in MB"
+	FROM information_schema.TABLES
+	GROUP BY table_schema ;
