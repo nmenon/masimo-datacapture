dbdd576b8d60b768e83120a709bd0f6e1d4bf47b
Nishanth Menon <menon.nishanth@gmail.com>
2016-04-05 00:28:10 -0500

    elasticsearch: fix up help..
    Copy paste with sense.. use $0 for script name in help..
    
    Signed-off-by: Nishanth Menon <menon.nishanth@gmail.com>

--- elasticsearch/get_db_size.sh
+++ elasticsearch/get_db_size.sh
@@ -5,12 +5,12 @@ INDEXNAME=$1
 HOSTNAME_AND_PORT=$2
 
 if [ -z $INDEXNAME ]; then
-  echo "usage:  create_index_and_mappings.sh <indexname> <host:9200>"
+  echo "usage:  $0 <indexname> <host:9200>"
   exit
 fi
 
 if [ -z $HOSTNAME_AND_PORT ]; then
-  echo "usage:  create_index_and_mappings.sh <indexname> <host:9200>"
+  echo "usage:  $0 <indexname> <host:9200>"
   exit
 fi
 
