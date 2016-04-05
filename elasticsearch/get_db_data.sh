4f0db92c9f6dd80a1d10d27e0fadbd7c952948f9
Nishanth Menon <menon.nishanth@gmail.com>
2016-04-05 00:32:18 -0500

    elasticsearch: add a script to pull in the data
    When your mind tells you something that your brain wont figure out..
    best to look at the actual json data itself.
    
    Signed-off-by: Nishanth Menon <menon.nishanth@gmail.com>

---
+++ elasticsearch/get_db_data.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Shamelessly copied from:
+# https://github.com/yahnatan/pulseox/blob/master/create_index_and_mappings.sh
+INDEXNAME=$1
+HOSTNAME_AND_PORT=$2
+
+if [ -z $INDEXNAME ]; then
+  echo "usage:  $0 <indexname> <host:9200>"
+  exit
+fi
+
+if [ -z $HOSTNAME_AND_PORT ]; then
+  echo "usage:  $0 <indexname> <host:9200>"
+  exit
+fi
+
+curl -XGET "http://$HOSTNAME_AND_PORT/$INDEXNAME/"'_search?q=*&pretty'
+
+
+echo
